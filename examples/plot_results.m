78ebc4f7820201d2dd5a636ce8ade793d2a38eab
Vicenç Gómez <vicen.gomez@upf.edu>
2017-02-22 22:58:05 +0100

    changes

--- examples/plot_results.m
+++ examples/plot_results.m
@@ -1,3 +1,15 @@
-data = csvread('/home/vgomez/rllab/data/local/experiment/experiment_2017_02_21_16_57_45_0001/progress.csv',1,0);
-plot(data(:,10))
+data_trpo = csvread('/home/vgomez/rllab/data/local/experiment/experiment_2017_02_21_16_57_45_0001/progress.csv',1,0);
+data_npireps = csvread('/home/vgomez/rllab/data/local/experiment/experiment_2017_02_21_16_53_50_0001/progress.csv',1,0);
+
+h = figure(1);
+clf;
+plot(data_npireps(:,3))
+hold on;
+plot(data_trpo(:,13))
 set(gca,'xscale','log');
+title('Average Return')
+xlabel('Iteration')
+
+legend('npireps','trpo','location','northwest')
+
+saveas(h,'fig1.eps','psc2')
