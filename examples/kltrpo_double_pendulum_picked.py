2f818baa41da11826fd46bdfaac2be07aa28701b
Vicenç Gómez <vicen.gomez@upf.edu>
2017-02-22 23:00:10 +0100

    added kltrpo example

---
+++ examples/kltrpo_double_pendulum_picked.py
@@ -0,0 +1,40 @@
+
+from rllab.misc.instrument import run_experiment_lite
+from rllab.algos.npireps import NPIREPS
+from rllab.sampler.pi_sampler import PISampler
+from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+from rllab.envs.box2d.double_pendulum_env import DoublePendulumEnv
+from rllab.envs.normalized_env import normalize
+from rllab.policies.gaussian_mlp_policy import GaussianMLPPolicy
+
+def run_task(*_):
+    env = normalize(DoublePendulumEnv())
+
+    print("Action dims = " + str(env.action_dim))
+    print("obs dim = " + str(env.observation_space.flat_dim))
+
+    policy = GaussianMLPPolicy(
+        env_spec=env.spec,
+    )
+
+    baseline = LinearFeatureBaseline(env_spec=env.spec)
+
+    algo = NPIREPS(
+        env=env,
+        policy=policy,
+        baseline=baseline,
+        sampler_cls=PISampler,
+        kl_trpo=True
+    )
+    algo.train()
+
+run_experiment_lite(
+    run_task,
+    # Number of parallel workers for sampling
+    n_parallel=5,
+    # Only keep the snapshot parameters for the last iteration
+    snapshot_mode="last",
+    # Specifies the seed for the experiment. If this is not provided, a random seed
+    # will be used
+    seed=1,
+)
