3f9f3263fb78808e0ce7843c032fa3a21aa33c21
Vicenç Gómez <vicen.gomez@upf.edu>
2017-02-24 12:39:13 +0100

    fixed line search

--- examples/plot_results.m
+++ examples/plot_results.m
@@ -41,8 +41,8 @@ npireps_0_1_avg_return_2N = get_data('/home/vgomez/rllab/data/local/experiment/e
 
 kl_trpo_total_cost_unc05 = get_data('/home/vgomez/rllab/data/local/experiment/experiment_2017_02_23_22_11_58_0001/progress.csv','Total cost');
 kl_trpo_avg_return_unc05 = get_data('/home/vgomez/rllab/data/local/experiment/experiment_2017_02_23_22_11_58_0001/progress.csv','AverageReturn');
-npireps_0_01_total_cost_unc05 = get_data('/home/vgomez/rllab/data/local/experiment/experiment_2017_02_23_22_17_38_0001/progress.csv','Total cost');
-npireps_0_01_avg_return_unc05 = get_data('/home/vgomez/rllab/data/local/experiment/experiment_2017_02_23_22_17_38_0001/progress.csv','AverageReturn');
+npireps_0_05_total_cost_unc05 = get_data('/home/vgomez/rllab/data/local/experiment/experiment_2017_02_23_22_17_38_0001/progress.csv','Total cost');
+npireps_0_05_avg_return_unc05 = get_data('/home/vgomez/rllab/data/local/experiment/experiment_2017_02_23_22_17_38_0001/progress.csv','AverageReturn');
 npireps_0_1_total_cost_unc05 = get_data('/home/vgomez/rllab/data/local/experiment/experiment_2017_02_23_22_12_51_0001/progress.csv','Total cost');
 npireps_0_1_avg_return_unc05 = get_data('/home/vgomez/rllab/data/local/experiment/experiment_2017_02_23_22_12_51_0001/progress.csv','AverageReturn');
 npireps_0_15_total_cost_unc05 = get_data('/home/vgomez/rllab/data/local/experiment/experiment_2017_02_23_22_21_21_0001/progress.csv','Total cost');
@@ -100,7 +100,7 @@ clf;
 subplot(1,2,1);
 plot(kl_trpo_total_cost_unc05);
 hold on;
-plot(npireps_0_01_total_cost_unc05);
+plot(npireps_0_05_total_cost_unc05);
 plot(npireps_0_1_total_cost_unc05);
 plot(npireps_0_15_total_cost_unc05);
 grid on;
@@ -111,13 +111,13 @@ subplot(1,2,2);
 plot(kl_trpo_avg_return_unc05)
 ylabel('Average Reward');
 hold on;
-plot(npireps_0_01_avg_return_unc05);
+plot(npireps_0_05_avg_return_unc05);
 plot(npireps_0_1_avg_return_unc05);
 plot(npireps_0_15_avg_return_unc05);
 
 grid on;
-legend('KL-TRPO','Nat-PIREPS \delta=0.1','Nat-PIREPS \delta=0.2','Nat-PIREPS \delta=0.25','Nat-PIREPS \delta=0.3','location','southeast')
-saveas(h,'fig1.eps','psc2')
+legend('KL-TRPO','Nat-PIREPS \delta=0.05','Nat-PIREPS \delta=0.1','Nat-PIREPS \delta=0.15','Nat-PIREPS \delta=0.3','location','southeast')
+saveas(h,'fig2.eps','psc2')
 
 
 
@@ -147,7 +147,7 @@ plot(npireps_0_3_avg_return_2N);
 
 grid on;
 legend('KL-TRPO','Nat-PIREPS \delta=0.1','Nat-PIREPS \delta=0.2','Nat-PIREPS \delta=0.25','Nat-PIREPS \delta=0.3','location','southeast')
-saveas(h,'fig1.eps','psc2')
+saveas(h,'fig3.eps','psc2')
 
 
 
@@ -181,7 +181,7 @@ plot(npireps_0_05_avg_return_eps_0_01);
 
 grid on;
 legend('KL-TRPO','Nat-PIREPS \delta=0.4','Nat-PIREPS \delta=0.3','Nat-PIREPS \delta=0.2','Nat-PIREPS \delta=0.1','Nat-PIREPS \delta=0.05, \epsilon=0.05','Nat-PIREPS \delta=0.05, \epsilon=0.01','location','southeast')
-saveas(h,'fig2.eps','psc2')
+saveas(h,'fig4.eps','psc2')
 
 
 end
